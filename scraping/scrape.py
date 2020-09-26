aeac680f99725dee33c66a198caa796198c0a72e
Raven <farahmakram@gmail.com>
2020-09-26 21:20:17 +0100

    scrape pages 1 and 2 of hacker news

--- scraping/scrape.py
+++ scraping/scrape.py
@@ -1,8 +1,9 @@
 import requests
 from bs4 import BeautifulSoup
+import pprint #pretty print
 
 res = requests.get('https://news.ycombinator.com/news')
-print(res)
+# print(res)
 # grab the data only in string format, don't need styling info
 # print(res.text)
 # give string, and convert type (html/xml), creates soup object
@@ -31,18 +32,51 @@ votes = soup.select('.score')
 # votes.get to get attribute
 
 # href attribute is actual link
-def create_custom_hm(links, votes):
+
+subtext = soup.select('.subtext')
+
+# get url of next page, a is hyperlink tag
+next_page_url = soup.find_all('a', {'class': 'morelink', 'rel': 'next'})
+# print(next_page_url[0].get('href'))
+if next_page_url:
+    next_page_url = 'https://news.ycombinator.com/' + next_page_url[0].get('href')
+    # print(next_page_url)
+
+res2 = requests.get(next_page_url)
+soup2 = BeautifulSoup(res2.text, 'html.parser')
+# print(soup2)
+
+links2 = soup2.select('.storylink')
+subtext2 = soup.select('.subtext')
+
+mega_links = links + links2
+mega_subtext = subtext + subtext2
+
+def sort_stories_by_votes(hnlist):
+    return sorted(hnlist, key=lambda k:k['votes'], reverse=True)
+
+
+def create_custom_hm(links, subtext):
     hn = []
+    # enumerate to have index for subtext
     for index, item in enumerate(links):
 
         title = links[index].getText()
         # default second param in case no link
         href = links[index].get('href', None)
         # there are some stories that don't have votes, making votes list shorter than link list
-        points = int(votes[index].getText().replace(' points', ''))
-        print(points)
-        hn.append({'title': title, 'link': href})
-    return hn
+        vote = subtext[index].select('.score')
+        # if there is a vote attribute in the subtext, i.e. vote !=null
+        if len(vote):
+            points = int(vote[0].getText().replace(' points', ''))
+            # print(points)
+            if points > 99:
+                hn.append({'title': title, 'link': href, 'votes': points})
 
 
-print(create_custom_hm(links, votes))
+    return sort_stories_by_votes(hn)
+
+
+# print(create_custom_hm(links, subtext))
+pprint.pprint(create_custom_hm(mega_links, mega_subtext))
+
